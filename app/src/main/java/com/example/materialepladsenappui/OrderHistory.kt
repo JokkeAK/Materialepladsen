0f5c53d7db5fd26228eef35b08b40cc092839e7a
Joakim Anker Kruse <joakimkruse@hotmail.com>
2022-10-30 15:55:53 +0100

    Made a card for receipts, added the card to OrderHistory.kt, used a custom inner shadow functions for the list shadow edges.

--- app/src/main/java/com/example/materialepladsenappui/OrderHistory.kt
+++ app/src/main/java/com/example/materialepladsenappui/OrderHistory.kt
@@ -10,11 +10,13 @@ import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.surfaceColorAtElevation
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.composed
+import androidx.compose.ui.draw.drawWithContent
+import androidx.compose.ui.graphics.*
+import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -35,29 +37,105 @@ fun OrderHis(){
         Header()
 
         Text("Kvitteringer",
-            style = TextStyle(fontSize = 18.sp, fontWeight = FontWeight.Bold),
-            modifier = Modifier.padding(5.dp)
-        )
+            style = TextStyle(fontSize = 18.sp, fontWeight = FontWeight.Bold))
 
         LazyColumn(modifier = Modifier
             .background(color = Color.Gray, shape = RectangleShape)
             .fillMaxWidth()
-            .height(650.dp),
+            .height(650.dp)
+            .innerShadow(),
             contentPadding = PaddingValues(horizontal = 16.dp, vertical = 10.dp),
             horizontalAlignment = Alignment.CenterHorizontally){
 
-            item { BordeauxButton(text = "tester")
+            item { ReceiptCard(onClick = {} ) //need to handle on click in the future
             Spacer(modifier = Modifier.height(5.dp))}
-            items(20){index -> BordeauxButton(text = "tester $index")
+            items(20){index -> ReceiptCard(onClick = {} ) //need to handle on click in the future
                 Spacer(modifier = Modifier.height(5.dp))}
+        }
+    }
+}
 
+//Custom inner shadow code from https://stackoverflow.com/questions/71054138/jetpack-compose-inner-shadow
+//Modified to fit our app design
+//Inbuilt shadows for backgrounds suck.
+fun Modifier.innerShadow() = composed(
+    inspectorInfo = {
 
+    },
+    factory = {
+
+        val paint = remember() {
+            Paint()
         }
 
+        val foregroundPaint = remember() {
+            Paint().apply {
+                color = Color.Gray
+            }
+        }
 
+        val frameworkPaint = remember {
+            paint.asFrameworkPaint()
+        }
+
+        Modifier.drawWithContent {
+            this.drawIntoCanvas {
+                val color = Color(0x656464)
+
+                val radius = 8.dp.toPx()
+
+                val shadowColor = color
+                    .copy(alpha = .7f)
+                    .toArgb()
+                val transparent = color
+                    .copy(alpha = 0f)
+                    .toArgb()
+
+                frameworkPaint.color = transparent
+
+                frameworkPaint.setShadowLayer(
+                    radius,
+                    0f,
+                    0f,
+                    shadowColor
+                )
+                val shadowRadius = 8.dp.toPx()
+
+                it.drawRoundRect(
+                    left = 0f,
+                    top = 0f,
+                    right = this.size.width,
+                    bottom = this.size.height,
+                    radiusX = 5.dp.toPx(),
+                    radiusY = 5.dp.toPx(),
+                    paint = foregroundPaint
+                )
+
+                it.drawRoundRect(
+                    left = 0f,
+                    top = 0f,
+                    right = this.size.width,
+                    bottom = this.size.height,
+                    radiusX = 5.dp.toPx(),
+                    radiusY = 5.dp.toPx(),
+                    paint = paint
+                )
+
+                it.drawRoundRect(
+                    left = shadowRadius,
+                    top = shadowRadius,
+                    right = this.size.width - shadowRadius,
+                    bottom = this.size.height,
+                    radiusX = 5.dp.toPx(),
+                    radiusY = 5.dp.toPx(),
+                    paint = foregroundPaint
+                )
+                drawContent()
+
+            }
+        }
     }
-
-}
+)
 
 @Preview(showBackground = true)
 @Composable
