cc047585565d3340d9ae1dc6c13a125b6254d3d9
Martin Wenzel <s203867@student.dtu.dk>
2022-11-07 09:54:16 +0100

    Navbar started, can't really figure it out

--- app/src/main/java/com/example/materialepladsenappui/NavHost.kt
+++ app/src/main/java/com/example/materialepladsenappui/NavHost.kt
@@ -1,6 +1,25 @@
 package com.example.materialepladsenappui
 
 import androidx.compose.runtime.Composable
+import android.content.Context
+import android.content.Intent
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import kotlinx.coroutines.selects.select
 
 enum class Routes() {
     Landing,
@@ -8,7 +27,34 @@ enum class Routes() {
     BuyOnSite,
     Reciepts
 }
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun navBar(){
+
+    val navController = rememberNavController()
+
+    Scaffold(
+        bottomBar = {
+        navbar()}
+        ) { innerPadding ->
+        NavHost(navController = navController, startDestination = Routes.Landing.name,
+            modifier = Modifier.padding(innerPadding)) {
+                composable(route = Routes.Landing.name) {
+                MyScreenContent()
+            }
+        }
+
+
+    }
+}
+
+
+
+
+
+
+
+
+fun navbar() {
 
 }
