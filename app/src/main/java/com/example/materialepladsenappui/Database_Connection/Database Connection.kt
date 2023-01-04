91a46fdb5b85775fcca6bd08418a48deb8c44dd1
Noah Grænge Surel <s215783@student.dtu.dk>
2023-01-04 10:21:34 +0100

    Changed url

--- app/src/main/java/com/example/materialepladsenappui/Database_Connection/Database Connection.kt
+++ app/src/main/java/com/example/materialepladsenappui/Database_Connection/Database Connection.kt
@@ -5,8 +5,8 @@ import java.sql.DriverManager
 
 //The class that establishes a connection to the database.
 class DatabaseConnection {
-    private val url = "jdbc:sqlserver://<152.115.71.190>:<48123>;databaseName=<database>"
-    private val user = "<DTUMP2>"
+    private val url = "jdbc:sqlserver://152.115.71.190:48123/materialepladsen_core_DTUMP2"
+    private val user = "DTUMP2"
     private val password = "<mxDEKrZ8c2>"
 
     //A method that gets a connection to the database.
