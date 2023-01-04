0bec9d632b3aafd3d081dcd63aab559790dbdd07
Joakim Anker Kruse <joakimkruse@hotmail.com>
2023-01-04 10:11:24 +0100

    Added some comments to the database connection

--- app/src/main/java/com/example/materialepladsenappui/Database_Connection/Database Connection.kt
+++ app/src/main/java/com/example/materialepladsenappui/Database_Connection/Database Connection.kt
@@ -3,11 +3,13 @@ package com.example.materialepladsenappui.Database_Connection
 import java.sql.Connection
 import java.sql.DriverManager
 
+//The class that establishes a connection to the database.
 class DatabaseConnection {
     private val url = "jdbc:sqlserver://<152.115.71.190>:<48123>;databaseName=<database>"
     private val user = "<DTUMP2>"
     private val password = "<mxDEKrZ8c2>"
 
+    //A method that gets a connection to the database.
     fun getConnection(): Connection {
         return DriverManager.getConnection(url, user, password)
     }
