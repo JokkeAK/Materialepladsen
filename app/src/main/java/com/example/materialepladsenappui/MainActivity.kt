09355ad9794b00fb7c6ca091294567189f18989d
TheGreenLi0n <s200260@student.dtu.dk>
2023-01-04 11:22:09 +0100

    Trying java class as helper functions.

--- app/src/main/java/com/example/materialepladsenappui/MainActivity.kt
+++ app/src/main/java/com/example/materialepladsenappui/MainActivity.kt
@@ -4,10 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.example.composenavigation.ui.MainContent
+import com.example.materialepladsenappui.Database_Connection.DBHelper
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        DBHelper.selectSome()
+
         setContent {
             MainContent()
         }
