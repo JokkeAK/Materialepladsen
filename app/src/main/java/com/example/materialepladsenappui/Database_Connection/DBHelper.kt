0a6408775c11f4ec6f9585a6b0d38f79da852419
Joakim Anker Kruse <joakimkruse@hotmail.com>
2023-01-05 16:04:04 +0100

    Added comments and cleaned up code for the Database Connection.kt and DBHelper.kt

--- app/src/main/java/com/example/materialepladsenappui/Database_Connection/DBHelper.kt
+++ app/src/main/java/com/example/materialepladsenappui/Database_Connection/DBHelper.kt
@@ -1,36 +1,35 @@
 package com.example.materialepladsenappui.Database_Connection
 
-import android.content.ContentValues.TAG
-import android.nfc.Tag
 import android.util.Log
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
-import java.sql.Connection
 
 class DBHelper {
-
- fun Select() {
+    // Method to select a record from the database
+    fun Select() {
+        // Create a DatabaseConnection object and get a connection
         val db = DatabaseConnection()
-        val conn = db.getAltConnection()
-     if (conn != null) {
-         if(conn.isClosed){
-             Log.i("Conn", "Connection is closed")
-         } else{
-             Log.i("Conn","Connection is not closed " + conn.toString())
-         }
+        val conn = db.getConnection()
 
-     } else{
-         Log.i("Conn","Connection is null")
-     }
+        // Check if the connection is not null and not closed
+        if (conn != null) {
+            if (conn.isClosed) {
+                Log.i("Conn", "Connection is closed")
+            } else {
+                Log.i("Conn", "Connection is not closed " + conn.toString())
+            }
+        } else {
+            Log.i("Conn", "Connection is null")
+        }
 
+        // Set the query to select a record from the v_mobileApp_products table
         val query = "SELECT TOP (1) [product_group] FROM [materialepladsen_core_DTUMP2].[dbo].[v_mobileApp_products]"
 
+        // Create a statement and execute the query
         val stmt = conn?.createStatement()
-        Log.i("Conn","after statement")
+        Log.i("Conn", "after statement")
         val rs = stmt?.executeQuery(query)
         Log.i("Conn", "after executeQuery")
-        //val tableName = rs?.getString(1)
+
+        // Log the result set
         Log.i("Conn", rs.toString())
     }
-
 }
--- app/src/main/java/com/example/materialepladsenappui/Database_Connection/Database Connection.kt
+++ app/src/main/java/com/example/materialepladsenappui/Database_Connection/Database Connection.kt
@@ -3,47 +3,46 @@ package com.example.materialepladsenappui.Database_Connection
 import android.os.StrictMode
 import android.util.Log
 import java.sql.Connection
-import java.sql.Driver
 import java.sql.DriverManager
 import java.sql.SQLException
 
 //The class that establishes a connection to the database.
 class DatabaseConnection {
+    // JDBC URL and credentials for connecting to the database
     private val url = "jdbc:sqlserver://152.115.71.190:48123;databaseName=materialepladsen_core_DTUMP2"
     private val user = "DTUMP2"
     private val password = "mxDEKrZ8c2"
 
-    //A method that gets a connection to the database.
-    fun getConnection(): Connection {
-        return DriverManager.getConnection(url, user, password)
-
-    }
-
-    fun getAltConnection() : Connection?{
+    // Method to get an connection to the database
+    fun getConnection(): Connection? {
+        // Set the thread policy to allow all connections
         val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
         StrictMode.setThreadPolicy(policy)
-        var conn : Connection? = null
-        var connString : String? = null
+
+        // Initialize the connection and connection string variables
+        var conn: Connection? = null
+        var connString: String? = null
         try {
-            //Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver")
-           // connString =
-                //"jdbc:jtds:sqlserver://152.115.71.190:48123;databaseName=materialepladsen_core_DTUMP2;user=DTUMP2;password=mxDEKrZ8c2;"
-
-           // Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver")
+            // Set the connection string for the alternative connection
             connString = "jdbc:jtds:sqlserver://152.115.71.190:48123;database=materialepladsen_core_DTUMP2;user=DTUMP2;password=mxDEKrZ8c2;"
-
+            // Attempt to get the connection using the DriverManager
             conn = DriverManager.getConnection(connString)
-        }
-        catch (ex : SQLException) {
+        } catch (ex: SQLException) {
+            // Log any SQL exceptions that occur
             ex.message?.let { Log.e("Error : ", it) }
-        } catch (ex1 : ClassNotFoundException) {
+        } catch (ex1: ClassNotFoundException) {
+            // Log any ClassNotFoundExceptions that occur
             ex1.message?.let { Log.e("Error : ", it) }
-        } catch (ex2 : java.lang.Exception) {
+        } catch (ex2: java.lang.Exception) {
+            // Log any other exceptions that occur
             ex2.message?.let { Log.e("Error : ", it) }
         }
-
-
+        // Return the connection (null if an exception occurred)
         return conn
-        }
     }
+}
+
+
+
+
 
