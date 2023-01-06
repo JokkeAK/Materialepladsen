8ab0d7b5d80182e022265cc6fc0bd3d852b79dd9
TheGreenLi0n <s200260@student.dtu.dk>
2023-01-06 10:46:50 +0100

    Added model and viewmodel packages. and made first attempt at ProductModel

--- app/src/main/java/com/example/materialepladsenappui/MainActivity.kt
+++ app/src/main/java/com/example/materialepladsenappui/MainActivity.kt
@@ -14,12 +14,10 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-
         GlobalScope.async {
             val dbhelper = DBHelper()
             Log.i("hello", "main:ahhhhhhhhhhhh ")
             dbhelper.Select() }
-
 
         setContent {
             MainContent()
---
+++ app/src/main/java/com/example/materialepladsenappui/Models/ProductModel.kt
@@ -0,0 +1,49 @@
+package com.example.materialepladsenappui.Models
+
+class ProductModel(){
+    val productGroup: String
+        get() {
+            TODO()
+        }
+    val id: Long
+        get() {
+            TODO()
+        }
+    val productArticleNumber: String
+        get() {
+            TODO()
+        }
+    val productArticleName: String
+        get() {
+            TODO()
+        }
+    val productSize: String
+        get() {
+            TODO()
+        }
+    val productWeight: Double
+        get() {
+            TODO()
+        }
+    val productPriceExTAX: Double
+        get() {
+            TODO()
+        }
+    val productPriceInTAX: Double
+        get() {
+            TODO()
+        }
+    val unit: String
+        get() {
+            TODO()
+        }
+    val availability: Boolean
+        get() {
+            TODO()
+        }
+    val productImage: String
+        get() {
+            TODO()
+        }
+
+}
