97ff3abdf93eed3393be380d2f27a99245d04ec9
Martin Wenzel <s203867@student.dtu.dk>
2023-01-08 16:47:05 +0100

    Deserialize almost don, need a few more functions, and some models to be looked at, And made a change to the (communist) android manifesto

--- app/src/main/java/com/example/materialepladsenappui/json/JsonDeserializer.kt
+++ app/src/main/java/com/example/materialepladsenappui/json/JsonDeserializer.kt
@@ -1,9 +1,27 @@
 package com.example.materialepladsenappui.json
+import android.util.Log
+import com.example.materialepladsenappui.models.OrdersModel
 import com.google.gson.Gson
 import java.net.URL
 class JsonDeserializer {
 
 
+
+    fun test(){
+
+        val webPage = URL(
+            "http://152.115.71.190:41000/?user=DTUMP2&password=mxDEKrZ8c2&request=EC02E425-B6BD-4D82-A9A2-" +
+                "F58507385B41&type=requestOrderNew&customerid=12537&licenseplate=DB22100")
+
+        val data = webPage.readText()
+
+       // val gson = Gson()
+       // val td = gson.fromJson(data)
+
+        Log.i("JSONTEST", data)
+
+
+    }
 }
 
 
