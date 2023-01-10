2875431938f9301519d50ab1d251564af8a477b9
TheGreenLi0n <s200260@student.dtu.dk>
2023-01-10 16:26:38 +0100

    try something so package get recognised

--- app/src/main/java/com/example/materialepladsenappui/models/Customer.kt
+++ app/src/main/java/com/example/materialepladsenappui/models/Customer.kt
@@ -2,25 +2,25 @@ package com.example.materialepladsenappui.models
 
 import java.util.Date
 
-data class CustomersModel(
-    val customerNumber: Int,
-    val customerGUID: Int,
+data class Customer(
+    val customerNumber: Int?,
+    val customerGUID: Int?,
     val customerFirstName: String,
     val customerLastName: String,
-    val customerAddress: String,
+    val customerAddress: String?,
     val customerPostalcode: String,
     val customerCity: String,
     val customerPhone: String,
     val customerMobile: String,
     val customerEmail: String,
     val customerPassword: String,
-    val cardNumber: String,
+    val cardNumber: String?,
     val licensplate: String,
-    val created: Date,
-    val createdBy: Date,
-    val modified: Date,
-    val modifiedBy: String,
-    val archived: Date,
-    val archivedBy: String
+    val created: Date?,
+    val createdBy: Date?,
+    val modified: Date?,
+    val modifiedBy: String?,
+    val archived: Date?,
+    val archivedBy: String?
 )
 
